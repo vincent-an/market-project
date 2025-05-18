1afb0aebb07948e87e8bafb08f196e106870f2a5
vincent-an <xxv211@naver.com>
2025-05-19 00:30:32 +0900

    first commit

---
+++ src/main/java/likeLion2025/Letf/LetfApplication.java
@@ -0,0 +1,13 @@
+package likeLion2025.Letf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetfApplication.class, args);
+	}
+
+}
