79dc4a57b29ba52b040d4d365a2ea4e112a5d187
seo061010 <seo061010@g.eulji.ac.kr>
2025-06-26 17:40:09 +0900

    first commit

---
+++ src/test/java/likeLion2025/Letf/LetfApplicationTests.java
@@ -0,0 +1,13 @@
+package likeLion2025.Letf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
